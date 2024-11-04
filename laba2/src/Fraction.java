423ad5ad03583e69a6fc5f8f8ce680721ef2dd9b
HintMSI <86974891+HintMSI@users.noreply.github.com>
2024-11-05 00:43:48 +0500

    Add files via upload

---
+++ laba2/src/Fraction.java
@@ -0,0 +1,36 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction sum(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction minus(int number) {
+        int newNumerator = this.numerator - number * this.denominator;
+        return new Fraction(newNumerator, this.denominator);
+    }
+
+    public Fraction del(Fraction other) {
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction umnojenie(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
